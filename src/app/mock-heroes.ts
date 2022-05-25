4e4dd834a3005d71df2f720990b368ec5e93c3e8
Ariel Andrielli Rodrigues da Silva <arielandrielli100@gmail.com>
2022-05-25 16:20:42 -0300

    Iniciando Repositorio

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 2, name: 'Dr. Strange' },
+  { id: 3, name: 'Batman' },
+  { id: 4, name: 'Hulk' },
+  { id: 5, name: 'Magneto' },
+  { id: 6, name: 'RubberMan' },
+  { id: 7, name: 'Wolverine' },
+  { id: 8, name: 'Shazam' },
+  { id: 9, name: 'Thor' },
+  { id: 10, name: 'GreenLantern' }
+];
